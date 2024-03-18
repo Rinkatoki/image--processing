5935783c4587b4da9a02f968e16cb90448ec081b
Manikandan <143013380+Rinkatoki@users.noreply.github.com>
2024-03-18 18:27:22 +0530

    second commit
    using only opencv

--- image processing.py
+++ image processing.py
@@ -1,24 +1,20 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 
 def edge_detection():
     gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray_image, 50, 150)
-    plt.subplot(121), plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)), plt.title('Original Image')
-    plt.subplot(122), plt.imshow(edges, cmap='gray'), plt.title('Edge Detection')
+    cv2.imshow("Edges",edges)
+    cv2.waitKey(0)
     plt.show()
     
 def image_rotation():
-    image = cv2.resize(original_image, (0, 0), fx = 0.1, fy = 0.1)
-    half = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-    cv2.imshow("image", half)
+    image = cv2.rotate(original_image, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imshow("image",image)
     cv2.waitKey(0)
 
 def gray_scaling():
     gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    half_gray = cv2.resize(gray_image, (0, 0),fx = 0.1,fy = 0.1)
-    cv2.imshow("grey image",half_gray)
+    cv2.imshow("grey image",gray_image)
     cv2.waitKey(0)
 
     
